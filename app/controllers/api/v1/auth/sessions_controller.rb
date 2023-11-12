e595ab024e2e9670b51e76bc711511540e4e72ca
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-11-13 04:30:42 +0900

    名前ログインできるように

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,10 +1,18 @@
 # 認証確認用コントローラー
 class Api::V1::Auth::SessionsController < ApplicationController
+  before_action :configure_permitted_parameters
+
   def index
     if current_api_v1_user
       render json: { status: 200, current_user: current_api_v1_user }
     else
       render json: { status: 500, message: "ユーザーが存在しません" }
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
